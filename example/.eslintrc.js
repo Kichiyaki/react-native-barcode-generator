84c0ea84da2954c7f361d5731c08c3458cebb676
Yudi Alviska <yudi.a@weefer.co.id>
2023-04-14 10:18:46 +0700

    feat: update example, update depedency, enhance code

--- example/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  rules: {
-    'no-shadow': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-    'react-native/no-inline-styles': 'off',
-  },
-};
